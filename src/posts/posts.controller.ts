36a59794eb93191c5c6e2868157f0bb66d1e973e
ubardev <bellin83@gmail.com>
2024-10-10 22:48:02 +0900

    :necktie: Feat: Get Posts 구현하기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -9,7 +9,8 @@ import { PostsService } from './posts.service';
  * commentCount: number;
  */
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -17,18 +18,39 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 100000000,
+    commentCount: 999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '노래 연습 하고 있는 해린',
+    likeCount: 100000000,
+    commentCount: 999999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '뉴진스 로제',
+    content: '종합운동장에서 공연중인 로제',
+    likeCount: 100000000,
+    commentCount: 999999,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 100000000,
-      commentCount: 999999,
-    };
+  getPost(): PostModel[] {
+    return posts;
   }
 }
