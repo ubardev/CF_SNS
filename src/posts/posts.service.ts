5610a0bff62b397d8d0ad96dcb4f38d7edc4a300
ubardev <bellin83@gmail.com>
2024-10-11 15:12:32 +0900

    :necktie: Feat: Create 함수로 새로운 데이터 생성하기

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -68,19 +68,18 @@ export class PostsService {
     return post;
   }
 
-  createPost(author: string, title: string, content: string) {
-    const post = {
-      id: posts[posts.length - 1].id + 1,
+  async createPost(author: string, title: string, content: string) {
+    const post = this.postsRepository.create({
       author,
       title,
       content,
       likeCount: 0,
       commentCount: 0,
-    };
+    });
 
-    posts = [...posts, post];
+    const newPost = await this.postsRepository.save(post);
 
-    return post;
+    return newPost;
   }
 
   updatePost(
