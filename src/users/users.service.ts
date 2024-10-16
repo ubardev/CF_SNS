7e4e8c7105299551941058bf3fb3d5a930449480
ubardev <bellin83@gmail.com>
2024-10-17 07:30:25 +0900

    :necktie: Feat: registerWithEmail 함수 정의하기

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersModel } from './entities/users.entity';
 import { Repository } from 'typeorm';
@@ -18,14 +18,30 @@ export class UsersService {
     return this.usersRepository.findOne({ where: { email } });
   }
 
-  async createUser(nickname: string, email: string, password: string) {
-    const user = this.usersRepository.create({
-      nickname,
-      email,
-      password,
+  async createUser(user: Pick<UsersModel, 'email' | 'nickname' | 'password'>) {
+    const nicknameExists = await this.usersRepository.exists({
+      where: { nickname: user.nickname },
     });
 
-    const newUser = await this.usersRepository.save(user);
+    if (nicknameExists) {
+      throw new BadRequestException('이미 존재하는 닉네임입니다.');
+    }
+
+    const emailExists = await this.usersRepository.exists({
+      where: { email: user.email },
+    });
+
+    if (emailExists) {
+      throw new BadRequestException('이미 존재하는 이메일입니다.');
+    }
+
+    const userObject = this.usersRepository.create({
+      nickname: user.nickname,
+      email: user.email,
+      password: user.password,
+    });
+
+    const newUser = await this.usersRepository.save(userObject);
 
     return newUser;
   }
