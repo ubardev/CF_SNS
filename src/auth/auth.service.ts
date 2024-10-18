90af3903a39b4f625b0158451d71a6d64ab28d0f
ubardev <bellin83@gmail.com>
2024-10-19 05:46:17 +0900

    :necktie: Feat: 토큰 시스템을 사용하도록 엔드포인트 변경하기

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -42,16 +42,40 @@ export class AuthService {
    * {authorization: 'Bearer {token}'}
    */
 
-  async extractTokenFromHeader(header: string, isBearer: boolean) {
+  extractTokenFromHeader(header: string, isBearer: boolean) {
     const splitToken = header.split(' ');
     const prefix = isBearer ? 'Bearer' : 'Basic';
 
     if (splitToken.length !== 2 || splitToken[0] !== prefix) {
-      throw new UnauthorizedException('토큰이 올바르지 않습니다.');
+      throw new UnauthorizedException('잘못된 토큰입니다!');
     }
 
     const token = splitToken[1];
     return token;
+  }
+
+  /**
+   * Basic asdfasdfasdfasdfasdfasdfasdf
+   * 1) asdfasdfasdfasdf -> email:password
+   * 2) email:password -> [email, password]
+   * 3) {email: email, password: password}
+   */
+  decodeBasicToken(base64String: string) {
+    const decoded = Buffer.from(base64String, 'base64').toString('utf8');
+
+    const split = decoded.split(':');
+
+    if (split.length !== 2) {
+      throw new UnauthorizedException('잘못된 유형의 토큰입니다!');
+    }
+
+    const email = split[0];
+    const password = split[1];
+
+    return {
+      email,
+      password,
+    };
   }
 
   /**
