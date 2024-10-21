162c07f8ef67aef658f2a3588b496a40aee2ca61
ubardev <bellin83@gmail.com>
2024-10-22 08:31:22 +0900

    :necktie: Feat: User 커스텀 데코레이터 생성하기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -14,6 +14,8 @@ import {
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
+import { User } from 'src/users/decorator/user.decorator';
+import { UsersModel } from 'src/users/entities/users.entity';
 
 @Controller('posts')
 export class PostsController {
@@ -34,13 +36,11 @@ export class PostsController {
   @Post()
   @UseGuards(AccessTokenGuard)
   postPosts(
-    @Request() req: any,
+    @User() user: UsersModel,
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const authorId = req.user.id;
-
-    return this.postsService.createPost(authorId, title, content);
+    return this.postsService.createPost(user.id, title, content);
   }
 
   @Put(':id')
