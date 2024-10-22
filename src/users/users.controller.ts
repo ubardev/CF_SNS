7597bc2380f9e04dc7a5f2a0567e16c79ed0b678
ubardev <bellin83@gmail.com>
2024-10-23 08:58:51 +0900

    :necktie: Feat: ClassSerializer AppModule에 적용해보기

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,7 +13,11 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  @UseInterceptors(ClassSerializerInterceptor)
+  /**
+   * serialization -> 직렬화 -> 현재 시스템에서 사용되는 (NestJS) 데이터의 구조를 다른 시스템에서도 쉽게 사용할 수 있는 포맷으로 변환
+   *                        -> class의 object에서 JSON 포맷으로 변환
+   * deserialization -> 역직렬화
+   */
   getUsers() {
     return this.usersService.getAllUsers();
   }
