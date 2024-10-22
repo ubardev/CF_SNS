12e583fd709ca25bb86090f5f32ddc97632dae58
ubardev <bellin83@gmail.com>
2024-10-22 23:25:01 +0900

    :necktie: Feat: Class Validator 에러 메세지 변경하기

--- src/posts/dto/create-post.dot.ts
+++ src/posts/dto/create-post.dot.ts
@@ -1,9 +1,13 @@
 import { IsString } from 'class-validator';
 
 export class CreatePostDto {
-  @IsString()
+  @IsString({
+    message: 'title은 string 타입을 입력 해줘야합니다.',
+  })
   title: string;
 
-  @IsString()
+  @IsString({
+    message: 'content은 string 타입을 입력 해줘야합니다.',
+  })
   content: string;
 }
