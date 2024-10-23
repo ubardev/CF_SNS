ebe858d81f6a74181c30712fc3946a505103440f
ubardev <bellin83@gmail.com>
2024-10-24 08:40:41 +0900

    :necktie: Feat: Post 응답 생성하기

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -47,7 +47,24 @@ export class PostsService {
     }
   }
 
-  async pagePaginatePosts(dto: PaginatePostDto) {}
+  async pagePaginatePosts(dto: PaginatePostDto) {
+    /**
+     * data: Data[],
+     * total: number,
+     * next: ??
+     */
+    const posts = await this.postsRepository.find({
+      skip: dto.take * (dto.page - 1),
+      take: dto.take,
+      order: {
+        createdAt: dto.order__created_at,
+      },
+    });
+
+    return {
+      data: posts,
+    };
+  }
 
   async cursorPaginatePosts(dto: PaginatePostDto) {
     const where: FindOptionsWhere<PostsModel> = {};
