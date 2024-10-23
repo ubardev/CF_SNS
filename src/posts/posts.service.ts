fd9381928727bd2824762c585c64c420f15d5948
ubardev <bellin83@gmail.com>
2024-10-24 08:29:29 +0900

    :necktie: Feat: 내림차순 정렬 Where 쿼리 작성하기

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -8,7 +8,7 @@
 
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { MoreThan, Repository } from 'typeorm';
+import { FindOptionsWhere, LessThan, MoreThan, Repository } from 'typeorm';
 import { PostsModel } from './entities/posts.entity';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
@@ -40,11 +40,17 @@ export class PostsService {
 
   // 1) 오름차 순으로 정렬하는 pagination만 구현한다
   async paginatePosts(dto: PaginatePostDto) {
+    const where: FindOptionsWhere<PostsModel> = {};
+
+    if (dto.where__id_less_than) {
+      where.id = LessThan(dto.where__id_less_than);
+    } else if (dto.where__id_more_than) {
+      where.id = MoreThan(dto.where__id_more_than);
+    }
+
+    // 1, 2, 3, 4, 5
     const posts = await this.postsRepository.find({
-      where: {
-        // 더 크다 / 더 많다
-        id: MoreThan(dto.where__id_more_than ?? 0),
-      },
+      where,
       // order__createdAt
       order: {
         createdAt: dto.order__created_at,
