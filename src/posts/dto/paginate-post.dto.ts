fbe48895a15b8081db435bbd75665891873aa7ca
ubardev <bellin83@gmail.com>
2024-10-24 08:24:16 +0900

    :necktie: Feat: 내림차순 Next 토큰 로직 작성하기

--- src/posts/dto/paginate-post.dto.ts
+++ src/posts/dto/paginate-post.dto.ts
@@ -2,17 +2,26 @@ import { Type } from 'class-transformer';
 import { IsIn, IsNumber, IsOptional } from 'class-validator';
 
 export class PaginatePostDto {
+  @IsNumber()
+  @IsOptional()
+  where__id_less_than?: number;
+
   // 이전 마지막 데이터의 ID
   // 이 프로퍼티에 입력된 ID 보다 높은 ID 부터 값을 가져오기
   @IsNumber()
   @IsOptional()
   where__id_more_than?: number;
 
+  // 10, 9, 8, 7
+  // 6, 5, 4, 3
+  // where__id_more_than = 7
+  // where__id_less_than = 7
+
   // 정렬
   // createdAt -> 생성된 시간의 내림차 / 오름차 순으로 정렬
-  @IsIn(['ASC'])
+  @IsIn(['ASC', 'DESC'])
   @IsOptional()
-  order__created_at?: 'ASC' = 'ASC';
+  order__created_at?: 'ASC' | 'DESC' = 'ASC';
 
   // 몇개의 데이터를 응답으로 받을지
   @IsNumber()
