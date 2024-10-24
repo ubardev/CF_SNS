1e390993b01af64c83a42aabcd7db03c98bc9ae1
ubardev <bellin83@gmail.com>
2024-10-25 08:42:24 +0900

    :necktie: Feat: Override Options 사용해보기

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -42,7 +42,12 @@ export class PostsService {
 
   // 1) 오름차 순으로 정렬하는 pagination만 구현한다
   async paginatePosts(dto: PaginatePostDto) {
-    return this.commonService.paginate(dto, this.postsRepository, {}, 'posts');
+    return this.commonService.paginate(
+      dto,
+      this.postsRepository,
+      { relations: ['author'] },
+      'posts',
+    );
     // if (dto.page) {
     //   return this.pagePaginatePosts(dto);
     // } else {
