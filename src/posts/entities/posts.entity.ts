273d367e8505091f966404239146d0f7ae99cb59
ubardev <bellin83@gmail.com>
2024-10-28 08:26:36 +0900

    :necktie: Feat: Class Transformer 이용해서 URL에 prefix 추가해주기

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,4 +1,7 @@
+import { Transform } from 'class-transformer';
 import { IsString } from 'class-validator';
+import { join } from 'path';
+import { POST_PUBLIC_IMAGE_PATH } from 'src/common/const/path.const';
 import { BaseModel } from 'src/common/entity/base.entity';
 import { stringValidationMessage } from 'src/common/validation-message/string-validation.message';
 import { UsersModel } from 'src/users/entities/users.entity';
@@ -27,6 +30,7 @@ export class PostsModel extends BaseModel {
   content: string;
 
   @Column({ nullable: true })
+  @Transform(({ value }) => value && `/${join(POST_PUBLIC_IMAGE_PATH, value)}`)
   image?: string;
 
   @Column()
