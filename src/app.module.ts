f72c7b5bfa9e2f99266915b42867574aca963f50
ubardev <bellin83@gmail.com>
2024-10-28 08:23:17 +0900

    :necktie: Feat: Static File Serving 옵션 추가하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,8 @@ import {
   ENV_DB_PORT_KEY,
   ENV_DB_USERNAME_KEY,
 } from './common/const/env-keys.const';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PUBLIC_FOLDER_PATH } from './common/const/path.const';
 
 @Module({
   imports: [
@@ -31,6 +33,13 @@ import {
       database: process.env[ENV_DB_DATABASE_KEY],
       entities: [PostsModel, UsersModel],
       synchronize: true,
+    }),
+    ServeStaticModule.forRoot({
+      // 4022.jpg
+      // http://localhost:3000/public/posts/4022.jpg
+      // http://localhost:3000/posts/4022.jpg
+      rootPath: PUBLIC_FOLDER_PATH,
+      serveRoot: '/public',
     }),
     PostsModule,
     UsersModule,
