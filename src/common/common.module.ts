9e612d6ed7a5fcae5fef13bc7062cf112eef9201
ubardev <bellin83@gmail.com>
2024-10-28 08:58:46 +0900

    :necktie: Feat: 이미지 업로드 엔드포인트 생성하기

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,57 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { CommonController } from './common.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { extname } from 'path';
+import * as multer from 'multer';
+import { POST_IMAGE_PATH, TEMP_FOLDER_PATH } from 'src/common/const/path.const';
+import { v4 as uuid } from 'uuid';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [
+    AuthModule,
+    UsersModule,
+    MulterModule.register({
+      limits: {
+        // 바이트 단위로 입력
+        fileSize: 10 * 1024 * 1024, // 10MB
+      },
+      fileFilter: (req, file, cb) => {
+        /**
+         * cb(에러, boolean)
+         *
+         * 첫번째 파라미터에는 에러가 있을 경우 에러 정보를 넣어준다.
+         * 두번째 파라미터에는 파일을 받을지 말지 boolean을 넣어준다.
+         */
+        const ext = extname(file.originalname);
+
+        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+          return cb(
+            new BadRequestException('jpg/jpeg/png 파일만 업로드 가능합니다!'),
+            false,
+          );
+        }
+
+        return cb(null, true);
+
+        // if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+        //   cb(null, true);
+        // } else {
+        //   cb(new Error('지원하지 않는 파일 형식입니다.'), false);
+        // }
+      },
+      storage: multer.diskStorage({
+        destination: function (req, res, cb) {
+          cb(null, TEMP_FOLDER_PATH);
+        },
+        filename: function (req, file, cb) {
+          cb(null, `${uuid()}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+  ],
   controllers: [CommonController],
   providers: [CommonService],
   exports: [CommonService],
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,11 +6,6 @@ import { PostsModel } from './entities/posts.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { UsersModule } from 'src/users/users.module';
 import { CommonModule } from 'src/common/common.module';
-import { MulterModule } from '@nestjs/platform-express';
-import { extname } from 'path';
-import * as multer from 'multer';
-import { POST_IMAGE_PATH } from 'src/common/const/path.const';
-import { v4 as uuid } from 'uuid';
 
 @Module({
   imports: [
@@ -18,44 +13,6 @@ import { v4 as uuid } from 'uuid';
     AuthModule,
     UsersModule,
     CommonModule,
-    MulterModule.register({
-      limits: {
-        // 바이트 단위로 입력
-        fileSize: 10 * 1024 * 1024, // 10MB
-      },
-      fileFilter: (req, file, cb) => {
-        /**
-         * cb(에러, boolean)
-         *
-         * 첫번째 파라미터에는 에러가 있을 경우 에러 정보를 넣어준다.
-         * 두번째 파라미터에는 파일을 받을지 말지 boolean을 넣어준다.
-         */
-        const ext = extname(file.originalname);
-
-        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
-          return cb(
-            new BadRequestException('jpg/jpeg/png 파일만 업로드 가능합니다!'),
-            false,
-          );
-        }
-
-        return cb(null, true);
-
-        // if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
-        //   cb(null, true);
-        // } else {
-        //   cb(new Error('지원하지 않는 파일 형식입니다.'), false);
-        // }
-      },
-      storage: multer.diskStorage({
-        destination: function (req, res, cb) {
-          cb(null, POST_IMAGE_PATH);
-        },
-        filename: function (req, file, cb) {
-          cb(null, `${uuid()}${extname(file.originalname)}`);
-        },
-      }),
-    }),
   ],
   controllers: [PostsController],
   providers: [PostsService],
