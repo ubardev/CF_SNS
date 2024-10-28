5bc6f97189d05548876e6699d23cfb39e622b5c8
ubardev <bellin83@gmail.com>
2024-10-29 08:24:24 +0900

    :necktie: Feat: ImageModel 만들기

---
+++ src/common/entity/image.entity.ts
@@ -0,0 +1,45 @@
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { BaseModel } from './base.entity';
+import { IsEnum, IsInt, IsOptional, IsString } from 'class-validator';
+
+import { POST_IMAGE_PATH } from '../const/path.const';
+import { Transform } from 'class-transformer';
+import { join } from 'path';
+import { PostsModel } from 'src/posts/entities/posts.entity';
+
+export enum ImageModelType {
+  POST_IMAGE,
+}
+
+@Entity()
+export class ImageModel extends BaseModel {
+  @Column({
+    default: 0,
+  })
+  @IsInt()
+  @IsOptional()
+  order: number;
+
+  // UserModel -> 사용자 프로필 이미지
+  // PostModel -> 포스트 이미지
+  @Column({
+    enum: ImageModelType,
+  })
+  @IsEnum(ImageModelType)
+  @IsString()
+  type: ImageModelType;
+
+  @Column()
+  @IsString()
+  @Transform(({ value, obj }) => {
+    if (obj.type === ImageModelType.POST_IMAGE) {
+      return join(POST_IMAGE_PATH, value);
+    } else {
+      return value;
+    }
+  })
+  path: string;
+
+  @ManyToOne((type) => PostsModel, (post) => post.images)
+  post?: PostsModel;
+}
--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -3,6 +3,7 @@ import { IsString } from 'class-validator';
 import { join } from 'path';
 import { POST_PUBLIC_IMAGE_PATH } from 'src/common/const/path.const';
 import { BaseModel } from 'src/common/entity/base.entity';
+import { ImageModel } from 'src/common/entity/image.entity';
 import { stringValidationMessage } from 'src/common/validation-message/string-validation.message';
 import { UsersModel } from 'src/users/entities/users.entity';
 import {
@@ -10,6 +11,7 @@ import {
   CreateDateColumn,
   Entity,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -29,13 +31,12 @@ export class PostsModel extends BaseModel {
   @Column()
   content: string;
 
-  @Column({ nullable: true })
-  @Transform(({ value }) => value && `/${join(POST_PUBLIC_IMAGE_PATH, value)}`)
-  image?: string;
-
   @Column()
   likeCount: number;
 
   @Column()
   commentCount: number;
+
+  @OneToMany((type) => ImageModel, (image) => image.post)
+  images: ImageModel[];
 }
