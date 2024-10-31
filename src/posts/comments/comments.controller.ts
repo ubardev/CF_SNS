f9abd91f53bebe67309ad8be02ad498443bb62a1
ubardev <bellin83@gmail.com>
2024-11-01 08:09:09 +0900

    :necktie: Feat: ID 기반으로 하나의 Comment 가져오는 API 작성하기

--- src/posts/comments/comments.controller.ts
+++ src/posts/comments/comments.controller.ts
@@ -1,24 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { CommentsService } from './comments.service';
+import { PaginateCommentsDto } from './dto/paginate-comments.dto';
 
 @Controller('posts/:postId/comments')
 export class CommentsController {
-  constructor(private readonly commentsService: CommentsService) {}
-  /**
-   * 1) Entity 생성
-   * author -> 작성자
-   * post -> 귀속되는 포스트
-   * comment -> 실제 댓글 내용
-   * likeCount -> 좋아요 갯수
-   *
-   * id -> PrimaryGeneratedColumn
-   * createdAt -> 생성일자
-   * updatedAt -> 업데이트 일자
-   *
-   * 2) GET() pagination
-   * 3) GET(:commentId) 특정 comment만 하나 가져오는 기능
-   * 4) POST() 코멘트 생성하는 기능
-   * 5) PATCH(':commentId') 특정 comment 업데이트 하는 기능
-   * 6) DELETE(':commentId') 특정 comment 삭제하는 기능
-   */
+  constructor(private readonly commentsService: CommentsService) {
+    /**
+     * 1) Entity 생성
+     * author -> 작성자
+     * post -> 귀속되는 포스트
+     * comment -> 실제 댓글 내용
+     * likeCount -> 좋아요 갯수
+     *
+     * id -> PrimaryGeneratedColumn
+     * createdAt -> 생성일자
+     * updatedAt -> 업데이트 일자
+     *
+     * 2) GET() pagination
+     * 3) GET(:commentId) 특정 comment만 하나 가져오는 기능
+     * 4) POST() 코멘트 생성하는 기능
+     * 5) PATCH(':commentId') 특정 comment 업데이트 하는 기능
+     * 6) DELETE(':commentId') 특정 comment 삭제하는 기능
+     */
+  }
+
+  @Get()
+  getComments(
+    @Param('postId', ParseIntPipe) postId: number,
+    @Query() query: PaginateCommentsDto,
+  ) {
+    return this.commentsService.paginateComments(query, postId);
+  }
+
+  @Get(':commentId')
+  getComment(@Param('commentId', ParseIntPipe) commentId: number) {
+    return this.commentsService.getCommentById(commentId);
+  }
 }
--- src/posts/comments/comments.service.ts
+++ src/posts/comments/comments.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CommonService } from 'src/common/common.service';
+import { PaginateCommentsDto } from './dto/paginate-comments.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommentsModel } from './entity/comments.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(
+    @InjectRepository(CommentsModel)
+    private readonly commentsRepository: Repository<CommentsModel>,
+    private readonly commonService: CommonService,
+  ) {}
+
+  paginateComments(dto: PaginateCommentsDto, postId: number) {
+    return this.commonService.paginate(
+      dto,
+      this.commentsRepository,
+      {
+        where: {
+          post: {
+            id: postId,
+          },
+        },
+      },
+      `posts/${postId}/comments`,
+    );
+  }
+
+  async getCommentById(id: number) {
+    const comment = await this.commentsRepository.findOne({ where: { id } });
+
+    if (!comment) {
+      throw new BadRequestException(`id: ${id} Comment는 존재하지 않습니다.`);
+    }
+
+    return comment;
+  }
+}
